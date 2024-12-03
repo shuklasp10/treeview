22aa7b17729b2bdec1cffc8882299570fa36826e
Shri Prakash Shukla <44600381+shuklasp10@users.noreply.github.com>
2024-12-03 10:32:45 +0000

    added collapse button

--- src/GenerateTree.jsx
+++ src/GenerateTree.jsx
@@ -2,15 +2,19 @@ import {useEffect, useState} from 'react';
 
 const GenerateTree = ({ root, parentSelected, depth }) => {
   const [isSelected, setIsSelected] = useState(false);
+  const [show, setShow] = useState(true);
+
   useEffect(()=>{
     setIsSelected(parentSelected);
   },[parentSelected])
+  
   return (
     <div  style={{marginLeft: 10*depth}}>
+      {root.children.length ? <button onClick={()=>setShow(!show)}>{show? "-":"+"}</button>:null}
       <input type="checkbox" onChange={()=>setIsSelected(!isSelected)} 
       checked={isSelected} name={root.name} id={root.name}/>
       <label htmlFor={root.name}>{root.name}</label>
-      {root.children && root.children.map((child)=>(
+      {(root.children && show) && root.children.map((child)=>(
         <GenerateTree root = {child} parentSelected = {isSelected} depth={depth+1}/>
       ))}
     </div>
